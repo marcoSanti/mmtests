30f4b704f93260d8a37782a26c9852349c825bb2
Marco Edoardo Santimaria <marco.santimaria@edu.unito.it>
2022-01-24 11:31:49 +0100

    added sysbenchcpu test support to multi-bench

--- drivers/driver-multi.sh
+++ drivers/driver-multi.sh
@@ -1,6 +1,7 @@
 $SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh netperf
 $SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh kernbench
 $SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh tbench4
+$SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh sysbenchcpu
 
 run_bench() {
 	$SHELLPACK_INCLUDE/shellpack-bench-multi
